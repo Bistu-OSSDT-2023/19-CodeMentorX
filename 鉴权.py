d6a7256a9fe60a6f1b5d38e00f9b1dcc35986f2d
XiaoGao <132557829+gaoxiao03@users.noreply.github.com>
2023-07-06 16:50:03 +0800

    Add files via upload

---
+++ 鉴权.py
@@ -0,0 +1,22 @@
+import time
+import jwt
+
+def generate_token(apikey: str, exp_seconds: int):
+    try:
+        id, secret = apikey.split(".")
+    except Exception as e:
+        raise Exception("invalid apikey", e)
+
+    payload = {
+        "api_key": id,
+        "exp": int(round(time.time() * 1000)) + exp_seconds * 1000,
+        "timestamp": int(round(time.time() * 1000)),
+    }
+
+    return jwt.encode(
+        payload,
+        secret,
+        algorithm="HS256",
+        headers={"alg": "HS256", "sign_type": "SIGN"},
+    )
+print(generate_token("2b1fe5cdbbf2f1ad6b737e1fbfbb87ba.E6a1ZVekEeNnXodT",3600))
